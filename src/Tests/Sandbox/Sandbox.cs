6d92460ddf47b8a3c6ace6146b8b135e5c39caf7
klubenov <pwnd0rum@gmail.com>
2018-12-11 20:02:39 +0200

    Added a sandbox tool for CRUD and testing outside of the WebApp.

---
+++ src/Tests/Sandbox/Sandbox.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Threading;
+using Foody.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Sandbox
+{
+    public static class Sandbox
+    {
+        public static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.WriteLine($"{typeof(Sandbox).Namespace} ({string.Join(" ", args)}) starts working...");
+            var serviceCollection = new ServiceCollection();
+            ConfigureServices(serviceCollection);
+            IServiceProvider serviceProvider = serviceCollection.BuildServiceProvider(true);
+
+            using (var serviceScope = serviceProvider.CreateScope())
+            {
+                serviceProvider = serviceScope.ServiceProvider;
+                SandboxCode(serviceProvider);
+            }
+        }
+
+        private static void SandboxCode(IServiceProvider serviceProvider)
+        {
+            Console.WriteLine("I am in the sandbox.");
+            Console.ReadLine();
+        }
+
+        private static void ConfigureServices(ServiceCollection services)
+        {
+            var configuration = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", false, true)
+                .AddEnvironmentVariables()
+                .Build();
+
+            services.AddDbContext<FoodyDbContext>(options =>
+                options.UseSqlServer(
+                    configuration.GetConnectionString("DefaultConnection")));
+        }
+    }
+}
